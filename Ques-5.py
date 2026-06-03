4c725528288214bee40898df787ed41233537af3
Harshit Mudgal <harshitmudgal647@gmail.com>
2026-06-03 09:39:40 +0530

    Add files via upload

---
+++ Ques-5.py
@@ -0,0 +1,15 @@
+#  Q5. Python program to create and display a list of fruits 
+
+# Ans.
+# making a list with 5 fruit names
+fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"]
+
+# printing the whole list
+print("Fruits Basket:", fruits)     ####👉👉 prints Fruits Basket: ['Apple', 'Banana', 'Mango', 'Orange', 'Grapes']
+
+# printing the first and last fruit
+print("First Fruit:", fruits[0])    ####👉👉 prints fruit at 0th index position "Apple"
+print("Last Fruit:", fruits[-1])    ####👉👉 prints fruit at last index position "Grapes"
+
+# printing the total number of fruits in the list
+print("Total items in Bucket:", len(fruits))    ####👉👉 Total Fruits in List: 5
