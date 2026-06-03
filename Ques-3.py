f6909aa85348dbeb223516a281e967d439d9773b
Harshit Mudgal <harshitmudgal647@gmail.com>
2026-06-03 09:35:43 +0530

    Update Ques-3.py

--- Ques-3.py
+++ Ques-3.py
@@ -2,10 +2,10 @@
 
 # Ans.
 # getting name from the user
-user_name = input("Enter your name: ")
+user_name = input("Enter your name: ")          ####👉👉 Takes user name as input
 
 # changing the name to uppercase and printing it
-print("Name in uppercase:", user_name.upper())
+print("Name in uppercase:", user_name.upper())      ####👉👉 prints user name in upper case
 
 # counting the characters using len() function
-print("Total number of characters:", len(user_name))
+print("Total number of characters:", len(user_name))    ####👉👉 prints total no. of characters in user name 
