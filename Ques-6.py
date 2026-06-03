9eecabeb0369fff643c6c4ba36d7877ee6ce4a95
Harshit Mudgal <harshitmudgal647@gmail.com>
2026-06-03 09:44:20 +0530

    Add files via upload

---
+++ Ques-6.py
@@ -0,0 +1,14 @@
+# Q6. Python program to modify a list of numbers
+
+#Ans.
+# creating list of numbers:
+numbers = [10, 20, 30, 40, 50]
+
+# adding 60 to the end of the list
+numbers.append(60)      ####👉👉 add 60 in the last position in list.
+
+# removing 20 from the list
+numbers.remove(20)      ####👉👉 removes 20 from the list
+
+# printing the final list to see the changes
+print("Updated list:", numbers)     ####👉👉 Updated list will be: [10, 30, 40, 50, 60]
