1f284cae2c92c17cbfb1475d9ca26012b1235481
Harshit Mudgal <harshitmudgal647@gmail.com>
2026-06-03 09:17:35 +0530

    Add files via upload

---
+++ Ques-2.py
@@ -0,0 +1,12 @@
+# Q2. Python program to create variables and print them
+
+# Ans.
+# making variables to store info
+name = "Harshit"
+age = 20
+city = "Jhunjhunu"
+
+# printing the variables as Per the Data provided.
+print("Name:", name)    ##### 👉👉 Prints name ---> "Harshit"  
+print("Age:", age)      ##### 👉👉 Prints age  --->  20 
+print("City:", city)    ##### 👉👉 Prints city ---> "Jhunjhunu"
