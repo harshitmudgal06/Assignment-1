73b807e8b186353821fc9914ac6c1b5b093e068a
Harshit Mudgal <harshitmudgal647@gmail.com>
2026-06-03 09:34:46 +0530

    Add files via upload

---
+++ Ques-3.py
@@ -0,0 +1,11 @@
+# Q3. Python program for user name input, uppercase, and length
+
+# Ans.
+# getting name from the user
+user_name = input("Enter your name: ")
+
+# changing the name to uppercase and printing it
+print("Name in uppercase:", user_name.upper())
+
+# counting the characters using len() function
+print("Total number of characters:", len(user_name))
